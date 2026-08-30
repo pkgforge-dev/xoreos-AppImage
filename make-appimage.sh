33a824cc41fe1a815cb5cef9e8b9e8ff7450b900
Link4Electronics <link4electronics@gmail.com>
2026-08-30 05:27:26 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,3 +14,7 @@ quick-sharun ./AppDir/bin/xoreos
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
